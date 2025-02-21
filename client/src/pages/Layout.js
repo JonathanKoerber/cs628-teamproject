49e1edd86e5ea95829d296a06054ac901df5319a
inderdeeprathore <147296641+inderdeeprathore@users.noreply.github.com>
2025-02-21 17:30:34 +0530

    Updated styling of Layout.js

--- client/src/pages/Layout.js
+++ client/src/pages/Layout.js
@@ -9,54 +9,78 @@ import Cookies from "js-cookie";
 import Signup from "../components/Signup";
 import LoginPage from "./LoginPage";
 //import {logout, verifyUser} from "../redux/user/authSlice";
+import '../Style/Layout.css'; 
 
 const Layout = () => {
-   const navigate = useNavigate();
-   const dispatch = useDispatch();
-   const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
-   const username = useSelector(state => state.auth.username);
-   const message = useSelector(state => state.auth.message);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+  const username = useSelector(state => state.auth.username);
+  const message = useSelector(state => state.auth.message);
 
-   useEffect(() => {
-      if(message != null){
-          toast.error(message, {position: "top-right"})
-      }
-   }, [message])
+  useEffect(() => {
+    if (message != null) {
+      toast.error(message, { position: "top-right" });
+    }
+  }, [message]);
 
-    useEffect(() => {
-        const tokenFromCookie = Cookies.get("token");
-            dispatch(validateUser())
-                .unwrap()
-                .then((response) => {
-                    console.log("validate user success", response)
-            }).catch(err => {
-                // navigate('/login')
-            });  // Redirect if verification fails
-        // navigate("/")
-    }, [isLoggedIn, dispatch, navigate]);
+  useEffect(() => {
+    const tokenFromCookie = Cookies.get("token");
+    dispatch(validateUser())
+      .unwrap()
+      .then((response) => {
+        console.log("validate user success", response);
+      }).catch(err => {
+        // navigate('/login');
+      });
+  }, [isLoggedIn, dispatch, navigate]);
 
-     const handleLogout = () => {
-        console.log("logout");
-        dispatch(logout());
-     }
+  const handleLogout = () => {
+    console.log("logout");
+    dispatch(logout());
+  };
 
-    return (
-        <>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/profile">Profile</Link></li>
-                    <li><Link to="/resume">Resume</Link></li>
-                    <li><Link to="/aires">AI Resume</Link></li>
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/not_a_rout">NoPage</Link></li>
-                </ul>
-            </nav>
-            <button className={"login-button"} onClick={() =>handleLogout()}>logout</button>
-            <h2>{username ? username : "No User"}</h2>
-            <Outlet />
-        </>
-    )
+  return (
+    <>
+      <div className="nav-container">
+        <nav className="navbar">
+       
+          <Link to="/">
+            <img src={logo} alt='Jazi AI Resume logo' className='logo' />
+
+          </Link>
+
+          
+          <div className="right-nav">
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/profile">Profile</Link></li>
+              <li><Link to="/resume">Resume</Link></li>
+              <li><Link to="/aires">AI Resume</Link></li>
+              {isLoggedIn ? (
+                <li>
+                  <button className="btn-signin" onClick={handleLogout}>
+                    Logout
+                  </button>
+                </li>
+              ) : (
+                <>
+                  <li><Link to="/signup">Sign Up</Link></li>
+                  <li><Link to="/login">Login</Link></li>
+                </>
+              )}
+            </ul>
+          </div>
+        </nav>
+      </div>
+
+      {/* Display username if logged in */}
+      <h2>{username ? username : ""}</h2>
+
+      {/* Route content will be rendered here */}
+      <Outlet />
+    </>
+  );
 };
 
 export default Layout;
