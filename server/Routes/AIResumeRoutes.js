2a6e41caa37eab16df786ced02dd59299e458530
aarthig0611 <aarthig0611@gmail.com>
2025-03-10 14:02:34 -0700

    Adding ai refine routes

--- server/Routes/AIResumeRoutes.js
+++ server/Routes/AIResumeRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { uploadResume, upload } = require('../Controlers/AIResumeController');
+const { uploadResume, upload, refineResume } = require('../Controlers/AIResumeController');
 
 const router = express.Router();
 
 router.post('/enhance', upload.single('resume'), uploadResume);
+router.post('/refine', refineResume);
 
 module.exports = router;
